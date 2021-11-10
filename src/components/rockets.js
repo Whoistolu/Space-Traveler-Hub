d7eca622a5e428ef4073d246773df581724185eb
Whoistolu <digittolu25@gmail.com>
2021-11-10 13:34:22 +0100

    Remove console log statement

--- src/components/rockets.js
+++ src/components/rockets.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 
 const Rockets = () => {
   const { rockets } = useSelector((state) => state);
-  console.log(rockets);
+  // console.log(rockets);
   return (
     <div>
       <p>Rockets</p>
